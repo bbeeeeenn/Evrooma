23637eecc5a6373cf2e0f519cb0c6db9266add2f
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-03-07 16:42:14 +0800

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,37 @@
 "use client";
-import { ArrowRight } from "lucide-react";
-import Link from "next/link";
+import { ArrowDown } from "lucide-react";
+import Image from "next/image";
 
 export default function Home() {
     return (
-        <div className="font-inter flex h-[200vh] flex-col items-center pt-40">
-            <h1 className="text-5xl font-bold tracking-widest sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl">
-                EVROOMA
-            </h1>
-            <p className="font-inria-sans mt-20 w-[80%] text-center text-3xl">
-                <strong>Find</strong> Available Classrooms{" "}
-                <strong>Instantly</strong>
-            </p>
-            <p className="mt-5 w-[80%] max-w-sm text-center text-xl">
-                Helps quickly identify available classrooms through a
-                centralized monitoring system designed for CCIS.
-            </p>
-            <br />
-            <p>(wait wala pa to)</p>
-            <Link
-                href={"/home"}
-                className="fixed inset-x-0 bottom-0 flex cursor-pointer items-center justify-center gap-3 bg-black px-20 py-5 font-bold tracking-wider text-white"
-            >
-                Continue <ArrowRight />
-            </Link>
-        </div>
+        <>
+            <section className="font-inter relative flex h-dvh flex-col items-center bg-black/5 pt-40">
+                <h1 className="text-5xl font-bold tracking-widest sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl">
+                    EVROOMA
+                </h1>
+                <p className="font-inria-sans mt-20 w-[80%] text-center text-3xl">
+                    <strong>Find</strong> Available Classrooms{" "}
+                    <strong>Instantly</strong>
+                </p>
+                <br />
+                <p className="opacity-30">(wait wala pa to)</p>
+                <button
+                    onClick={() => (window.location.href = "#roles")}
+                    className="inset-x-0 bottom-0 mt-5 flex cursor-pointer items-center gap-3 rounded-full bg-black px-20 py-3 font-bold tracking-wider text-white"
+                >
+                    Continue <ArrowDown />
+                </button>
+            </section>
+            <section id="roles" className="relative h-dvh bg-black">
+                <Image
+                    src={"/wave.svg"}
+                    alt={""}
+                    width={200}
+                    height={200}
+                    loading="eager"
+                    className="pointer-events-none absolute bottom-full w-full md:translate-y-1/8 lg:translate-y-1/7"
+                />
+            </section>
+        </>
     );
 }
