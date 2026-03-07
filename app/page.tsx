96fcf44adcb6051a691d75d792ccf3e567a9dd80
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-03-07 11:05:59 +0800

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,28 +1,16 @@
 "use client";
 import { ArrowRight } from "lucide-react";
-import Image from "next/image";
-import { useState } from "react";
+import RickRoll from "./components/RickRoll/RickRoll";
 
 export default function Home() {
-    const [rick, setRick] = useState(false);
     return (
         <div className="flex flex-col items-center pt-40">
             <h1 className="text-5xl font-bold">EVROOMA</h1>
-            <button
-                className="m-20 flex cursor-pointer items-center gap-3 rounded-full bg-black px-20 py-2 text-white"
-                onClick={() => setRick(true)}
-            >
-                Continue <ArrowRight />
-            </button>
-            {rick && (
-                <Image
-                    alt=""
-                    src={"/rickroll-rick.gif"}
-                    width={200}
-                    height={200}
-                    className="fixed inset-0 h-full w-full bg-black/70 object-contain"
-                />
-            )}
+            <RickRoll className="m-20">
+                <button className="flex cursor-pointer items-center gap-3 rounded-full bg-black px-20 py-2 text-white">
+                    Continue <ArrowRight />
+                </button>
+            </RickRoll>
         </div>
     );
 }
