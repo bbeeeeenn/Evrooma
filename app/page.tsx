4f00b4f3933186d66ea01ffc9528561f85e0ebbd
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-03-07 15:19:08 +0800

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,29 @@
 "use client";
 import { ArrowRight } from "lucide-react";
-import RickRoll from "./components/RickRoll/RickRoll";
+import Link from "next/link";
 
 export default function Home() {
     return (
-        <div className="flex flex-col items-center pt-40">
-            <h1 className="text-5xl font-bold tracking-widest">EVROOMA</h1>
-
-            <RickRoll className="m-20">
-                <button className="flex cursor-pointer items-center gap-3 rounded-full bg-black px-20 py-2 text-white">
-                    Continue <ArrowRight />
-                </button>
-            </RickRoll>
+        <div className="font-inter flex h-[200vh] flex-col items-center pt-40">
+            <h1 className="text-5xl font-bold tracking-widest sm:text-6xl md:text-7xl lg:text-8xl xl:text-9xl">
+                EVROOMA
+            </h1>
+            <p className="font-inria-sans mt-20 w-[80%] text-center text-3xl">
+                <strong>Find</strong> Available Classrooms{" "}
+                <strong>Instantly</strong>
+            </p>
+            <p className="mt-5 w-[80%] max-w-sm text-center text-xl">
+                Helps quickly identify available classrooms through a
+                centralized monitoring system designed for CCIS.
+            </p>
+            <br />
+            <p>(wait wala pa to)</p>
+            <Link
+                href={"/home"}
+                className="fixed inset-x-0 bottom-0 flex cursor-pointer items-center justify-center gap-3 bg-black px-20 py-5 font-bold tracking-wider text-white"
+            >
+                Continue <ArrowRight />
+            </Link>
         </div>
     );
 }
