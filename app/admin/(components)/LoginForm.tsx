b6e015181c16ff32e86782b46861f2853687e704
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-03-10 11:57:29 +0800

    .

--- app/admin/(components)/LoginForm.tsx
+++ app/admin/(components)/LoginForm.tsx
@@ -2,19 +2,31 @@
 
 import { AdminAuthAction } from "@/app/actions/AdminAuth";
 import clsx from "clsx";
-import { Lock, User } from "lucide-react";
-import { useActionState } from "react";
+import {
+    Eye,
+    EyeClosed,
+    Flashlight,
+    FlashlightOff,
+    Lock,
+    User,
+} from "lucide-react";
+import { ChangeEvent, useActionState, useState } from "react";
 
 export default function AdminLoginForm({
     action,
 }: {
     action: AdminAuthAction;
 }) {
-    const [formState, formAction, isPending] = useActionState(action, null);
+    const [prevFormData, formAction, isPending] = useActionState(action, null);
+
+    const [showPassword, setShowPassword] = useState(false);
+    const onShowPassword = (e: ChangeEvent<HTMLInputElement>) => {
+        setShowPassword(e.target.checked);
+    };
     return (
         <form
             action={formAction}
-            className="text-md font-poppins w-[80%] max-w-sm space-y-10 rounded-xl p-12 shadow-lg"
+            className="text-md font-poppins w-full max-w-md space-y-10 rounded-xl p-8 py-10 sm:shadow-lg"
         >
             <div className="flex items-center gap-3">
                 <label htmlFor="username">
@@ -27,9 +39,11 @@ export default function AdminLoginForm({
                         id="username"
                         placeholder="Username"
                         required
+                        autoComplete="off"
                         defaultValue={
-                            (formState?.formData.get("username") as string) ??
-                            ""
+                            (prevFormData?.formData.get(
+                                "username",
+                            ) as string) ?? ""
                         }
                         className="peer block w-full border-b-2 border-gray-300 py-1 placeholder-transparent focus:border-gray-600 focus:outline-0"
                     />
@@ -45,18 +59,27 @@ export default function AdminLoginForm({
                 <label htmlFor="password">
                     <Lock />
                 </label>
-                <div className="relative w-full">
+                <div className="relative flex w-full items-center gap-2 border-b-2 border-gray-300 focus-within:border-gray-600">
                     <input
-                        type="password"
+                        type={showPassword ? "text" : "password"}
                         name="password"
                         id="password"
                         placeholder="Password"
                         required
                         defaultValue={
-                            (formState?.formData.get("password") as string) ??
-                            ""
+                            (prevFormData?.formData.get(
+                                "password",
+                            ) as string) ?? ""
                         }
-                        className="peer block w-full border-b-2 border-gray-300 py-1 placeholder-transparent focus:border-gray-600 focus:outline-0"
+                        autoComplete="off"
+                        className="peer grow py-1 placeholder-transparent focus:outline-0"
+                    />
+                    <input
+                        type="checkbox"
+                        id="showpassword"
+                        defaultChecked={showPassword}
+                        className="hidden"
+                        onChange={onShowPassword}
                     />
                     <label
                         htmlFor="password"
@@ -64,16 +87,26 @@ export default function AdminLoginForm({
                     >
                         Password
                     </label>
+                    <label
+                        htmlFor="showpassword"
+                        className="cursor-pointer pr-2"
+                    >
+                        {showPassword ? (
+                            <Flashlight className="-rotate-90 text-gray-600 transition-all duration-200 peer-placeholder-shown:text-gray-400 peer-focus:text-gray-600" />
+                        ) : (
+                            <FlashlightOff className="text-gray-600 transition-all duration-200 peer-placeholder-shown:text-gray-400 peer-focus:text-gray-600" />
+                        )}
+                    </label>
                 </div>
             </div>
-            {!isPending && formState?.status === "error" && (
+            {!isPending && prevFormData?.status === "error" && (
                 <p className="mb-3 text-center text-sm text-red-600">
-                    {formState?.message}
+                    {prevFormData?.message}
                 </p>
             )}
             <button
                 className={clsx(
-                    "bg-black-200 text-black-100 w-full cursor-pointer rounded-md py-2 focus:outline-0",
+                    "bg-black-400 text-black-100 w-full cursor-pointer rounded-md py-2 focus:outline-0",
                     isPending && "opacity-50",
                 )}
                 disabled={isPending}
