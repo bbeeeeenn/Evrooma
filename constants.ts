a1e1326949c8c4f366a3c72367ce7fe83cb80df7
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-03-15 00:29:57 +0800

    .

--- constants.ts
+++ constants.ts
@@ -2,10 +2,10 @@ export const homePage = "/";
 
 export const adminLoginPage = "/admin";
 export const adminDashboardPage = "/admin/dashboard";
+export const adminRoomsPage = "/admin/dashboard/rooms";
+export const adminAccountsPage = "/admin/dashboard/accounts";
+export const adminChartsPage = "/admin/dashboard/charts";
 export const adminLogoutPage = "/admin/logout";
-export const adminRoomsPage = "/admin/rooms";
-export const adminAccountsPage = "/admin/accounts";
-export const adminChartsPage = "/admin/charts";
 
 export const instructorLoginPage = "/instructor";
 export const instructorDashboardPage = "/instructor/dashboard";
