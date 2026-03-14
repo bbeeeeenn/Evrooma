80cc89dd39fab5e35cae24bfbaee2f14918766a0
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-03-14 23:53:16 +0800

    .

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse, ProxyConfig } from "next/server";
 
 export async function proxy(_: NextRequest) {
-    await new Promise((resolve) => setTimeout(resolve, 500)); // simulate a lag
+    // await new Promise((resolve) => setTimeout(resolve, 500)); // simulate a lag
     return NextResponse.next();
 }
 
