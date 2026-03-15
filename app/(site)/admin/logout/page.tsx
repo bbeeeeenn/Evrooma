898e8c540002d29b9cab79011a1be20ad4b21179
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-03-15 22:09:55 +0800

    .

--- app/(site)/admin/logout/page.tsx
+++ app/(site)/admin/logout/page.tsx
@@ -1,16 +1,12 @@
 import AdminLogout from "./_";
 import { LogoutAdmin } from "@/app/actions/AdminActions";
-import CheckAuthentication from "@/app/components/CheckAuthentication";
-import { adminLoginPage } from "@/constants";
 import Loading from "../../loading";
 
 export default function AdminLogoutPage() {
     return (
-        <CheckAuthentication fallbackRoute={adminLoginPage}>
-            <div className="fixed inset-0 m-auto flex size-fit items-center gap-2 text-3xl font-bold">
-                <AdminLogout action={LogoutAdmin} />
-                <Loading text="Logging Out..." />
-            </div>
-        </CheckAuthentication>
+        <div className="fixed inset-0 m-auto flex size-fit items-center gap-2 text-3xl font-bold">
+            <AdminLogout action={LogoutAdmin} />
+            <Loading text="Logging Out..." />
+        </div>
     );
 }
