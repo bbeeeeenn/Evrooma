1a37f9e308306780758071e1edb1c47c6f5768ee
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-03-15 18:11:16 +0800

    .

--- app/(site)/loading.tsx
+++ app/(site)/loading.tsx
@@ -2,7 +2,7 @@ import Image from "next/image";
 
 export default function Loading() {
     return (
-        <div className="bg-black-100 dark:bg-black-400 dark:text-black-100 font-poppins fixed inset-0 m-auto flex flex-col items-center justify-center gap-3 text-4xl font-semibold tracking-widest">
+        <div className="bg-black-100 dark:bg-black-400 dark:text-black-100 font-inter fixed inset-0 m-auto flex flex-col items-center justify-center gap-3 text-4xl font-semibold tracking-widest">
             <Image
                 src="/favicon_light.svg"
                 width={200}
@@ -17,7 +17,7 @@ export default function Loading() {
                 alt="logo"
                 className="hidden animate-bounce dark:block"
             />
-            Loading...
+            EVROOMA
         </div>
     );
 }
