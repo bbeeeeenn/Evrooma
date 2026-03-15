578f7471cc3995152546e718d239fb5536e94ea4
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-03-15 22:00:33 +0800

    .

--- app/(site)/admin/dashboard/layout.tsx
+++ app/(site)/admin/dashboard/layout.tsx
@@ -2,12 +2,10 @@ import { AuthenticateAdmin } from "@/app/actions/AdminActions";
 import { adminLoginPage } from "@/constants";
 import { redirect } from "next/navigation";
 import { AdminNavBar } from "./AdminNavBar";
-import { Suspense } from "react";
-import Loading from "../../loading";
 
-const Suspended = async ({
+export default async function AdminLayout({
     children,
-}: Readonly<{ children: React.ReactNode }>) => {
+}: Readonly<{ children: React.ReactNode }>) {
     const user = await AuthenticateAdmin();
 
     if (!user) {
@@ -19,15 +17,5 @@ const Suspended = async ({
             <AdminNavBar />
             {children}
         </>
-    );
-};
-
-export default async function AdminLayout({
-    children,
-}: Readonly<{ children: React.ReactNode }>) {
-    return (
-        <Suspense fallback={<Loading text="Initializing..." />}>
-            <Suspended>{children}</Suspended>
-        </Suspense>
     );
 }
