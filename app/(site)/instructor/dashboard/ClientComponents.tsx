c4c8389cf9237fcd30988fb22b9a90293fa5c3d6
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-04-23 20:23:14 +0800

    feat: refactor InstructorNavBar for improved structure and readability

--- app/(site)/instructor/dashboard/ClientComponents.tsx
+++ app/(site)/instructor/dashboard/ClientComponents.tsx
@@ -22,71 +22,67 @@ export function InstructorNavBar() {
     const pathname = usePathname();
 
     return (
-        <>
-            <nav
+        <nav
+            className={clsx(
+                "bg-green-quarternary border-yellow-secondary fixed inset-x-0 bottom-0 z-50 m-auto flex justify-evenly border-t-2 pt-3 pb-2 font-bold tracking-wide text-white shadow-md transition-transform sm:inset-x-1/12 sm:bottom-4 sm:max-w-xl sm:rounded-xl sm:border-2 sm:py-4",
+                !shown && "translate-y-full sm:translate-y-[calc(100%+1rem)]",
+            )}
+        >
+            <button
                 className={clsx(
-                    "bg-green-quarternary border-yellow-secondary fixed inset-x-0 bottom-0 m-auto flex justify-evenly border-t-2 pt-3 pb-2 font-bold tracking-wide text-white shadow-md transition-transform sm:inset-x-1/12 sm:bottom-4 sm:max-w-xl sm:rounded-xl sm:border-2 sm:py-4",
-                    !shown &&
-                        "translate-y-full sm:translate-y-[calc(100%+1rem)]",
+                    "absolute bottom-full mb-1 cursor-pointer rounded-full border-0 p-1 backdrop-blur-xs transition-transform",
+                    shown ? "rotate-180" : "animate-bounce",
                 )}
+                onClick={() => setShown((prev) => !prev)}
             >
-                <button
-                    className={clsx(
-                        "absolute bottom-full mb-1 cursor-pointer rounded-full border-0 p-1 backdrop-blur-xs transition-transform",
-                        shown ? "rotate-180" : "animate-bounce",
-                    )}
-                    onClick={() => setShown((prev) => !prev)}
-                >
-                    <ChevronUp size={30} />
-                </button>
+                <ChevronUp size={30} />
+            </button>
 
-                <div className="flex grow">
-                    <Link
-                        href={
-                            pathname.includes(instructorSchedulesPage)
-                                ? instructorDashboardPage
-                                : instructorSchedulesPage
-                        }
-                        tabIndex={shown ? 0 : -1}
-                        className={clsx(
-                            "group m-auto flex cursor-pointer flex-col items-center gap-2 text-xs decoration-2 underline-offset-4 sm:flex-row sm:gap-3 sm:text-base",
-                            pathname.includes(instructorSchedulesPage) &&
-                                "underline",
-                        )}
-                    >
-                        <CalendarCheck className="transition-transform group-hover:scale-110 group-focus:scale-110 group-active:scale-110" />{" "}
-                        My Schedules
-                    </Link>
-                </div>
-                <div className="flex grow">
-                    <Link
-                        href={
-                            pathname.includes(instructorRoomsPage)
-                                ? instructorDashboardPage
-                                : instructorRoomsPage
-                        }
-                        tabIndex={shown ? 0 : -1}
-                        className={clsx(
-                            "group m-auto flex cursor-pointer flex-col items-center gap-2 text-xs decoration-2 underline-offset-4 sm:flex-row sm:gap-3 sm:text-base",
-                            pathname.includes(instructorRoomsPage) &&
-                                "underline",
-                        )}
-                    >
-                        <DoorClosed className="transition-transform group-hover:scale-110 group-focus:scale-110 group-active:scale-110" />{" "}
-                        Rooms
-                    </Link>
-                </div>
-                <div className="flex grow">
-                    <Link
-                        href={instructorLogoutPage}
-                        tabIndex={shown ? 0 : -1}
-                        className="group m-auto flex cursor-pointer flex-col items-center gap-2 text-xs decoration-2 sm:flex-row sm:gap-3 sm:text-base"
-                    >
-                        <SquareArrowRightExit className="transition-transform group-hover:scale-110 group-focus:scale-110 group-active:scale-110" />{" "}
-                        Logout
-                    </Link>
-                </div>
-            </nav>
-        </>
+            <div className="flex grow">
+                <Link
+                    href={
+                        pathname.includes(instructorSchedulesPage)
+                            ? instructorDashboardPage
+                            : instructorSchedulesPage
+                    }
+                    tabIndex={shown ? 0 : -1}
+                    className={clsx(
+                        "group m-auto flex cursor-pointer flex-col items-center gap-2 text-xs decoration-2 underline-offset-4 sm:flex-row sm:gap-3 sm:text-base",
+                        pathname.includes(instructorSchedulesPage) &&
+                            "underline",
+                    )}
+                >
+                    <CalendarCheck className="transition-transform group-hover:scale-110 group-focus:scale-110 group-active:scale-110" />{" "}
+                    My Schedules
+                </Link>
+            </div>
+            <div className="flex grow">
+                <Link
+                    href={
+                        pathname.includes(instructorRoomsPage)
+                            ? instructorDashboardPage
+                            : instructorRoomsPage
+                    }
+                    tabIndex={shown ? 0 : -1}
+                    className={clsx(
+                        "group m-auto flex cursor-pointer flex-col items-center gap-2 text-xs decoration-2 underline-offset-4 sm:flex-row sm:gap-3 sm:text-base",
+                        pathname.includes(instructorRoomsPage) && "underline",
+                    )}
+                >
+                    <DoorClosed className="transition-transform group-hover:scale-110 group-focus:scale-110 group-active:scale-110" />{" "}
+                    Rooms
+                </Link>
+            </div>
+            <div className="flex grow">
+                <Link
+                    href={instructorLogoutPage}
+                    tabIndex={shown ? 0 : -1}
+                    className="group m-auto flex cursor-pointer flex-col items-center gap-2 text-xs decoration-2 sm:flex-row sm:gap-3 sm:text-base"
+                >
+                    <SquareArrowRightExit className="transition-transform group-hover:scale-110 group-focus:scale-110 group-active:scale-110" />{" "}
+                    Logout
+                </Link>
+            </div>
+        </nav>
     );
 }
