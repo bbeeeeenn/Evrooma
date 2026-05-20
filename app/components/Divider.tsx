666d06a1c1dc3e7792b3107395226d93f2e78319
TRANQUILZOIIP <tranquilzoiip@gmail.com>
2026-05-20 16:34:54 +0800

    feat: enhance Divider component with center alignment option and update styling in ScanLandingPage

--- app/components/Divider.tsx
+++ app/components/Divider.tsx
@@ -1,6 +1,13 @@
-export function Divider({ text }: { text?: string }) {
+import clsx from "clsx";
+
+export function Divider({ text, center }: { text?: string; center?: boolean }) {
     return (
-        <div className="relative my-10 flex items-center justify-center font-semibold sm:justify-start">
+        <div
+            className={clsx(
+                "relative my-10 flex items-center justify-center font-semibold",
+                !center && "sm:justify-start",
+            )}
+        >
             <div className="absolute inset-0 m-auto h-px rounded-full bg-green-100/50"></div>
             {text && (
                 <p className="bg-green-primary text-md absolute w-fit px-2 text-center tracking-wide text-green-100 sm:ml-10 sm:text-lg">
